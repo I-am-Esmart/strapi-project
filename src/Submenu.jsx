aee8c1864b7eee1cdda5e0424c4d5b8d32e2ca71
Emmanuel Joseph <emmanueljoseph1320@gmail.com>
2023-06-09 22:42:52 +0100

    side bar complete

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,7 +1,35 @@
-import React from "react"
+import { useGlobalContext } from "./Context"
+import sublinks from "./data"
 
 const Submenu = () => {
-  return <div>Submenu</div>
+  const { pageId } = useGlobalContext
+
+  const currentPage = sublinks.find((item) => item.pageId === pageId)
+  console.log(currentPage)
+
+  return (
+    <div className="submenu">
+      <h5> {currentPage?.page} </h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+        {/* <h1>submenu</h1> */}
+      </div>
+    </div>
+  )
 }
 
 export default Submenu
